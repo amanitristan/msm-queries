3fd138c766c7a384733a87df33b7dcfe90e91d8a
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-10 03:04:44 +0000

    Edited directors_controller/youngest code

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,19 @@ class DirectorsController < ApplicationController
     render({ template: "director_templates/list" })
   end
 
+  def youngest
+    list_of_directors = Director.all
+    directors_with_dob = list_of_directors.where.not({ director_dob: "nil" })
+    ordered_directors = directors_with_dob.order({ director_dob: :asc })
+    @youngest_director = ordered_directors.at(0)
+
+    render({ template: "director_templates/youngest"})
+  end
+
+  def eldest
+    render({ template: "director_templates/eldest"})
+  end
+
   def show
     the_id = params.fetch("the_id")
 
